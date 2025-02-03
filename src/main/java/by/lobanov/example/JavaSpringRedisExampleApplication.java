eab2e889f365d67fbe9efc04558ca4f5954c0065
GitLobanov <lobanovgithub@gmail.com>
2025-02-03 15:35:16 +0300

    first commit

---
+++ src/main/java/by/lobanov/example/JavaSpringRedisExampleApplication.java
@@ -0,0 +1,13 @@
+package by.lobanov.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRedisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringRedisExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/lobanov/example/JavaSpringRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobanov.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
